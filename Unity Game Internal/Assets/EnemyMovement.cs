c7fba2297cac444eb5728681202a01f343fcda0e
LizardlordNetwork <pikadrewgaming1231@gmail.com>
2022-09-28 09:55:06 +1300

    isuufhyy qgfyqw

--- Unity Game Internal/Assets/EnemyMovement.cs
+++ Unity Game Internal/Assets/EnemyMovement.cs
@@ -18,6 +18,13 @@ public class EnemyMovement : MonoBehaviour
     //setting the force of the bullet
     public float bulletForce = 10f;
 
+    //Creating the enemies health and setting it as a variable. Making it a float to allow decimals.
+    private float Health = 0f;
+
+    //Making a private variable but setting it as a Serialize field to allow it to be edited inside the unity editor.
+    [SerializeField] private float maxHealth = 100f;
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
@@ -39,7 +46,8 @@ public class EnemyMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //setting health to the maximum health at the start of the game
+        Health = maxHealth;
     }
 
     // Update is called once per frame
@@ -109,5 +117,29 @@ public class EnemyMovement : MonoBehaviour
             }
             
         }
+    }
+
+    //Method for when health updates (gains or loses health) 
+    public void UpdateHealth(float modification)
+    {
+        //Adding the modifacation  to the health
+        Health += modification;
+
+        //If the health is greater then the max health then setting it back to the max health
+        if (Health > maxHealth)
+        {
+            Health = maxHealth;
+        }
+        else if (Health <= 0f)
+        {
+            /*if health is equal to or less then 0 then setting it back to 0 so it doesnt cause issues with negatives
+            Health = 0f;*/
+            
+        }
+    }
+
+    void Die()
+    {
+        Destroy(GameObject);
     }
 }
